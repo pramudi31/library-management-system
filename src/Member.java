4842d64f67ddc0705b3a78e756421617edf758e5
pramudi <pererapramudi31@gmail.com>
2026-07-11 15:42:10 +0530

    member class added

---
+++ src/Member.java
@@ -0,0 +1,15 @@
+public class Member {
+    int memberID;
+    String name;
+
+    public Member(int memberID, String name){
+        this.memberID = memberID;
+        this.name = name;
+    }
+
+    public void display(){
+        System.out.println("Member ID: "+memberID);
+        System.out.println("Name: "+name);
+
+    }
+}
