e5a1b01a202cb1b28aead9df6425ecc59cb9cf28
pramudi <pererapramudi31@gmail.com>
2026-07-12 17:22:19 +0530

    library class and main added

---
+++ src/VersionA/Library.java
@@ -0,0 +1,23 @@
+package VersionA;
+
+public class Library {
+
+    Book[] books;
+    int count = 0;
+
+    public Library(int number){
+        books = new Book[number];
+    }
+
+    public void addBook(Book book){
+        books[count] = book;
+        count ++;
+    }
+
+    public void displayBook(){
+        for (int i = 0; i<count ; i++){
+            books[i].display();
+            System.out.println();
+        }
+    }
+}
---
+++ src/VersionA/Main.java
@@ -0,0 +1,54 @@
+package VersionA;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter number of books: ");
+        int n = input.nextInt();
+        input.nextLine();
+
+        Library library = new Library(n);
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.println("\nEnter details of Book " + (i + 1));
+
+            System.out.println("Enter Book ID: ");
+            int id = input.nextInt();
+            input.nextLine();
+
+            System.out.println("Enter Book Title: ");
+            String title = input.nextLine();
+
+            System.out.println("Enter Author: ");
+            String author = input.nextLine();
+
+            Book book = new Book(id, title, author);
+            library.addBook(book);
+        }
+
+        System.out.println("\nLibrary Books");
+        library.displayBook();
+
+        System.out.println("\nEnter Member Details");
+
+        System.out.println("Enter Member ID: ");
+        int memberID = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Enter Member Name: ");
+        String name = input.nextLine();
+
+        Member member = new Member(memberID, name);
+
+        System.out.println("\nMember Details");
+        member.display();
+
+        input.close();
+    }
+}
